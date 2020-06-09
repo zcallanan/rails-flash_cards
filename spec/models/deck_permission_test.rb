a3ae95f04295a4f6d87f511a83a3c0fd3434e975
Zach Callanan <caveofmonsters@gmail.com>
2020-06-09 19:47:35 +0200

    deck test adjustments in progress

--- spec/models/deck_permission_test.rb
+++ spec/models/deck_permission_test.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe Deck, type: :model do
   let!(:user) { create(:user) }
   let!(:deck) { create(:deck) }
+  let!(:deck_permission) { DeckPermission.new(user: user, deck: deck) }
 
   before { sign_in user }
   it "is valid with valid attributes" do
-    expect(DeckPermission.new(user: user, deck: deck)).to be_valid
+    expect(deck_permission).to be_valid
   end
+
+  # it "is valid with an access level assigned" do
+  # end
 end
