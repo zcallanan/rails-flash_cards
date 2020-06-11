6265692c424842fd3c602536b4c22b12ca54e985
Zach Callanan <caveofmonsters@gmail.com>
2020-06-11 20:00:17 +0200

    user_can_update? action policy for decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -2,4 +2,10 @@ class DecksController < ApplicationController
   def index
     @decks = policy_scope(Deck)
   end
+
+  def show
+    @deck = Deck.find(params[:id])
+    authorize @deck
+
+  end
 end
