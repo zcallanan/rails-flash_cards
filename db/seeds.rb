3eb599ac02f0bbf83fcf389ab586a1b3280a0ddc
Zach Callanan <caveofmonsters@gmail.com>
2020-06-11 22:34:57 +0200

    created a method to handle permissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-CollectionPermission.destroy_all
+ CollectionPermission.destroy_all
 DeckPermission.destroy_all
 Collection.destroy_all
 Deck.destroy_all
 User.destroy_all
+
+
+def generate_permissions(value, user, deck, collection)
+  deck_hash = { user: user, deck: deck, read_access: true }
+  collection_hash = { user: user, collection: collection, read_access: true }
+  if value == 'update'
+    deck_hash[:update_access] = true
+    collection_hash[:update_access] = true
+  elsif value == 'clone'
+    deck_hash[:clone_access] = true
+    collection_hash[:clone_access] = true
+  end
+
+  permission = false
+  until permission
+    deck_random = Deck.all.sample
+    if deck_random.user != user
+      DeckPermission.create!(deck_hash)
+      CollectionPermission.create!(collection_hash)
+      puts "read access given to #{deck_random}"
+      permission = true
+    end
+  end
+end
 
 
 15.times do
@@ -19,27 +35,10 @@ User.destroy_all
   collection = Collection.create!(user: user, deck: deck)
   DeckPermission.create!(user: user, deck: deck, read_access: true, update_access: true, clone_access: true)
   CollectionPermission.create!(user: user, collection: collection, read_access: true, update_access: true, clone_access: true)
-  if Deck.count >= 7
-    permission = false
-    until permission
-      deck_random = Deck.all.sample
-      if deck_random.user != user
-        DeckPermission.create!(user: user, deck: deck_random, read_access: true)
-        CollectionPermission.create!(user: user, collection: collection, read_access: true)
-        puts "read access given to #{deck_random}"
-        permission = true
-      end
-    end
-    permission = false
-    until permission
-      deck_random = Deck.all.sample
-      if deck_random.user != user
-        DeckPermission.create!(user: user, deck: deck_random, read_access: true, update_access: true)
-        CollectionPermission.create!(user: user, collection: collection, read_access: true, update_access: true)
-        puts "read & write access given to #{deck_random}"
-        permission = true
-      end
-    end
+  if Deck.count >= 9
+    generate_permissions('read', user, deck, collection)
+    generate_permissions('update', user, deck, collection)
+    generate_permissions('clone', user, deck, collection)
   end
 end
 
