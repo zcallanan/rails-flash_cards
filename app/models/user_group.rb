ae37d1cd5ae16de483538079eb2969781503da99
Zach Callanan <caveofmonsters@gmail.com>
2020-06-14 17:14:19 +0200

    set accepts_nested_attributes_for on user group model, saved params for collection and question set

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -10,5 +10,5 @@ class UserGroup < ApplicationRecord
   has_many :question_sets, through: :user_group_question_sets
   has_many :user_group_tag_sets
   has_many :tag_sets, through: :user_group_tag_sets
-  accepts_nested_attributes_for :decks
+  accepts_nested_attributes_for :decks, :collections, :question_sets, :tag_sets
 end
