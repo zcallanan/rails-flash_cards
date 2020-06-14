2281a0153e0cfb6004cf58cb348df7880d579054
Zach Callanan <caveofmonsters@gmail.com>
2020-06-14 15:55:00 +0200

    set columns as unique

--- app/models/deck_string.rb
+++ app/models/deck_string.rb
@@ -1,6 +1,5 @@
 class DeckString < ApplicationRecord
   belongs_to :deck
 
-  validates :language, :title, presence: true
-  # validates :title, uniqueness: true
+  validates :language, :title, presence: true, uniqueness: true
 end
--- app/models/question_set_string.rb
+++ app/models/question_set_string.rb
@@ -1,6 +1,5 @@
 class QuestionSetString < ApplicationRecord
   belongs_to :question_set
 
-  validates :language, :title, presence: true
-  # validates :title, uniqueness: true
+  validates :language, :title, presence: true, uniqueness: true
 end
