ac48bc7b0951612761f61f46a5e23588d4d7a3d9
Zach Callanan <caveofmonsters@gmail.com>
2020-06-16 18:04:06 +0200

    Continuing work on factoring in language. Fixed how seed associates strings with permissions

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,25 +3,47 @@ class DecksController < ApplicationController
   before_action :set_deck, only: %i[show]
   def index
     @user = current_user
+    # TODO: break out string definition into a generalized method
     # list of decks the user owns
     @decks_owned = policy_scope(Deck).where(user: @user)
+    @decks_owned_strings = []
+    @decks_owned.each do |deck|
+      next if @decks_owned_strings.pluck(:deck_id).include?(deck.id)
+
+      languages = deck.deck_strings.pluck(:language)
+      if languages.include?(@user.language)
+        deck.deck_strings.each { |string| @decks_owned_strings << string if string.language == @user.language }
+      else
+        @decks_owned_strings << deck.deck_string[0]
+      end
+    end
     # list of decks the user can read but does not own
     @decks_read = policy_scope(Deck)
                   .joins(:deck_permissions)
                   .where({ deck_permissions: { user_id: @user.id, read_access: true } })
-                  .where.not(user: @user)
+                  .where.not(user: @user).distinct
     @decks_read_strings = []
     @decks_read.each do |deck|
-      deck.deck_strings.each do |string|
-        string.deck_permissions.each do |permission|
-          @decks_read_strings << string if string.language == permission.language && permission.user_id == @user.id
+      next if @decks_read_strings.pluck(:deck_id).include?(deck.id)
+
+      languages = deck.deck_strings.pluck(:language)
+      if languages.include?(@user.language)
+        deck.deck_strings.each do |string|
+          string.deck_permissions.each do |permission|
+            if string.language == @user.language && string.language == permission.language && permission.user_id == @user.id
+              @decks_read_strings << string
+            end
+          end
         end
+      else
+        @decks_owned_strings << deck.deck_string[0]
       end
     end
 
+    # TODO: Account for language
     # list of decks that are globally available
     @decks_global_strings = []
-    @decks_global = policy_scope(Deck).where(global_deck_read: true)
+    @decks_global = policy_scope(Deck).where(global_deck_read: true).distinct
     @decks_global.each do |deck|
       deck.deck_strings.each do |string|
         @decks_global_strings << string if string.global_access == true
