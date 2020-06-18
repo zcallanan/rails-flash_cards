5ea6492ab4d5bf1348e9a9d19efd1d62bb4d3a4b
Zach Callanan <caveofmonsters@gmail.com>
2020-06-18 15:59:16 +0200

    Fixed deck and collection create working with nested attributes. Creating initial permissions.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,21 +1,35 @@
 class CollectionsController < ApplicationController
-  before_action :set_deck, only: %i[show]
+  before_action :set_deck, only: %i[show create]
   before_action :set_collection, only: %i[show]
   def show
     authorize @collection
   end
 
   def create
+    @user = current_user
     @collection = Collection.new(collection_params)
+    @collection.user = @user
+    @collection.deck = @deck
     authorize @collection
-    # prepare simple_field usage
-    @collection.collection_strings.build
+    if @collection.save!
+      CollectionPermission.create!(
+        collection: @collection,
+        user: @user,
+        collection_string: @collection.collection_strings.first,
+        read_access: true,
+        update_access: true,
+        clone_access: true
+      )
+      redirect_to deck_collection_path(@deck, @collection)
+    else
+      redirect_to deck_path(@deck)
+    end
   end
 
   private
 
   def collection_params
-    params.require(:collection).permit(:collection_id, :deck_id, :language, :title, :description)
+    params.require(:collection).permit(collection_strings_attributes: [:language, :title, :description])
   end
 
   def set_deck
--- db/migrate/20200605094909_create_decks.rb
+++ db/migrate/20200605094909_create_decks.rb
@@ -1,6 +1,7 @@
 class CreateDecks < ActiveRecord::Migration[6.0]
   def change
     create_table :decks do |t|
+      t.string :name
       t.references :user, null: false, foreign_key: true
       t.boolean :global_deck_read, default: false
       t.boolean :archived, default: false
