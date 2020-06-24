6259087391932d65b8386d06e6ffeb69fd8d70f7
Zach Callanan <caveofmonsters@gmail.com>
2020-06-24 15:16:14 +0200

    condensed deck inline js to one action

--- app/javascript/controllers/deck_string_inline_controller.js
+++ app/javascript/controllers/deck_string_inline_controller.js
@@ -11,11 +11,13 @@ export default class extends Controller {
     "description",
     "edit",
     "div",
-    "submit"
+    "submit",
+    "button"
   ]
 
   dclick(event) {
-    const deckSubmitButton = event.target
+    const deckSubmitButton = this.submitTarget // if user clicks on the icon
+    const deckButton = this.buttonTarget // if user hits enter in the form
     const deckT = this.titleinfoTarget
     const deckDes = this.descriptioninfoTarget
     const deckTField = this.titleTarget
@@ -23,104 +25,44 @@ export default class extends Controller {
     const deckEditButton = this.editTarget
     const deckDiv = this.divTarget
 
-    setTimeout(function(){
-      if (isVisible(deckT) && isVisible(deckDes) && isVisible(deckEditButton)) {
+    if (isVisible(deckT) && isVisible(deckDes) && isVisible(deckEditButton)) {
+      setTimeout(function(){
         if (deckDiv.style.display == 'none') {
           deckT.style.display = 'none';
           deckDes.style.display = 'none';
           deckEditButton.style.display = 'none';
           deckDiv.style.display = 'block';
         }
-      }
-    }, 300);
-  }
-
-
-  dclickout(event) {
-    // mousedown event
-    const deckSubmitButton = this.submitTarget
-    const deckT = this.titleinfoTarget
-    const deckDes = this.descriptioninfoTarget
-    const deckTField = this.titleTarget
-    const deckDesField = this.descriptionTarget
-    const deckEditButton = this.editTarget
-    const deckDiv = this.divTarget
-    const deck_url = `http://localhost:3000/api/v1/decks/${deckSubmitButton.dataset.deck_id}/deck_strings/${deckSubmitButton.dataset.id}`;
-
-    if (!deckDiv.contains(event.target) && isVisible(deckTField) && isVisible(deckDesField)) {
-      if (deckDiv.style.display == 'block') {
-        deckDiv.style.display = 'none';
-        deckDes.style.display = 'block';
-        deckT.style.display = 'block';
-        deckEditButton.style.display = 'block';
-      }
-      fetchWithToken(deck_url,
-      {
-      method: "PATCH",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        deck_string: {
-          title: deckTField.value,
-          description: deckDesField.value
-        }
-      })
-    })
-      .then(response => response.json())
-      .then((data) => {
-        deckT.innerText = data.title;
-        deckDes.innerText = data.description;
-        deckSubmitButton.disabled = false;
-        if (deckDiv.style.display == 'block') {
-          deckDiv.style.display = 'none';
-          deckDes.style.display = 'block';
-          deckT.style.display = 'block';
-          deckEditButton.style.display = 'block';
-        }
-      });
+      }, 300);
     }
-
-  }
-
-  dsubmit(event) {
-    // form submission
-    event.preventDefault();
-
-    const deckSubmitButton = event.target
-    const deckT = this.titleinfoTarget
-    const deckDes = this.descriptioninfoTarget
-    const deckTField = this.titleTarget
-    const deckDesField = this.descriptionTarget
-    const deckEditButton = this.editTarget
-    const deckDiv = this.divTarget
-    const deck_url = `http://localhost:3000/api/v1/decks/${deckSubmitButton.dataset.deck_id}/deck_strings/${deckSubmitButton.dataset.id}`;
-
-    fetchWithToken(deck_url, {
-      method: "PATCH",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        deck_string: {
-          title: deckTField.value,
-          description: deckDesField.value
-        }
+    else if ((!deckDiv.contains(event.target) || (deckSubmitButton == event.target) || (deckButton == event.target)) && (isVisible(deckTField) && isVisible(deckDesField))) {
+      const deck_url = `http://localhost:3000/api/v1/decks/${deckSubmitButton.dataset.deck_id}/deck_strings/${deckSubmitButton.dataset.id}`;
+      event.preventDefault();
+      fetchWithToken(deck_url, {
+        method: "PATCH",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          deck_string: {
+            title: deckTField.value,
+            description: deckDesField.value
+          }
+        })
       })
-    })
-      .then(response => response.json())
-      .then((data) => {
-        deckT.innerText = data.title;
-        deckDes.innerText = data.description;
-        deckSubmitButton.disabled = false;
-        if (deckDiv.style.display == 'block') {
-          deckDiv.style.display = 'none';
-          deckDes.style.display = 'block';
-          deckT.style.display = 'block';
-          deckEditButton.style.display = 'block';
+        .then(response => response.json())
+        .then((data) => {
+          deckT.innerText = data.title;
+          deckDes.innerText = data.description;
+          deckSubmitButton.disabled = false;
+          if (deckDiv.style.display == 'block') {
+            deckDiv.style.display = 'none';
+            deckDes.style.display = 'block';
+            deckT.style.display = 'block';
+            deckEditButton.style.display = 'block';
+          }
+        });
     }
-      });
   }
 }
