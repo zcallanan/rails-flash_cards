e814b3d1b1930b3028e7155416b87ed9365c31a1
Zach Callanan <caveofmonsters@gmail.com>
2020-07-01 17:00:55 +0200

    Moved populate string private method to a concern. Used by two controllers

---
+++ app/controllers/concerns/populate_strings.rb
@@ -0,0 +1,42 @@
+class PopulateStrings
+  def initialize(attrs = {})
+    @objects = attrs[:objects]
+    @id_type = attrs[:id_type]
+    @string_type = attrs[:string_type]
+    @user = attrs[:user]
+    @permission_type = attrs[:permission_type]
+    @deck = attrs[:deck]
+  end
+
+  def call
+    # objects ~ decks, string_type ~ 'deck_strings', id_type ~ :deck_id, permission_type ~ 'deck_permissions'
+    object_strings = []
+    @objects.each do |object|
+      # prioritize strings that have a user's preferred language
+      next if object_strings.pluck(@id_type).include?(object.id)
+
+      languages = object.send(@string_type).pluck(:language)
+      if languages.include?(@user.language)
+        object.send(@string_type).each do |string|
+          if string.language == @user.language && @permission_type.nil?
+            object_strings << string
+          elsif string.language == @user.language
+            string.send(@permission_type).each do |permission|
+              object_strings << string if string.language == permission.language && permission.user_id == @user.id
+            end
+          end
+        end
+      else
+        # otherwise, return strings of the default language
+        object.send(@string_type).each do |string|
+          if string.language == object.default_language && @deck.nil?
+            object_strings << string
+          elsif string.language == object.send(@deck).default_language && !@deck.nil?
+            object_strings << string
+          end
+        end
+      end
+    end
+    object_strings
+  end
+end
--- app/controllers/shared_decks_controller.rb
+++ app/controllers/shared_decks_controller.rb
@@ -3,44 +3,15 @@ class SharedDecksController < ApplicationController
     @user = current_user
     # list of decks that are globally available
     @decks_global = policy_scope(Deck).where(global_deck_read: true, archived: false)
-    kwargs = {
-      objects: @decks_global, user: @user, string_type: 'deck_strings', id_type: :deck_id, permission_type: nil, deck: nil
+    deck_strings = {
+      objects: @decks_global,
+      user: @user,
+      string_type: 'deck_strings',
+      id_type: :deck_id,
+      permission_type: nil,
+      deck: nil
     }
-    @decks_global_strings = populate_strings(kwargs)
-
-  end
-
-  private
-
-  def populate_strings(**kwargs)
-    # objects ~ decks, string_type ~ 'deck_strings', id_type ~ :deck_id, permission_type ~ 'deck_permissions'
-    object_strings = []
-    kwargs[:objects].each do |object|
-      # prioritize strings that have a user's preferred language
-      next if object_strings.pluck(kwargs[:id_type]).include?(object.id)
-
-      languages = object.send(kwargs[:string_type]).pluck(:language)
-      if languages.include?(kwargs[:user].language)
-        object.send(kwargs[:string_type]).each do |string|
-          if string.language == kwargs[:user].language && kwargs[:permission_type].nil?
-            object_strings << string
-          elsif string.language == kwargs[:user].language
-            string.send(kwargs[:permission_type]).each do |permission|
-              object_strings << string if string.language == permission.language && permission.user_id == kwargs[:user].id
-            end
-          end
-        end
-      else
-        # otherwise, return strings of the default language
-        object.send(kwargs[:string_type]).each do |string|
-          if string.language == object.default_language && kwargs[:deck].nil?
-            object_strings << string
-          elsif string.language == object.send(kwargs[:deck]).default_language && !kwargs[:deck].nil?
-            object_strings << string
-          end
-        end
-      end
-    end
-    object_strings
+    # app/controllers/concerns/populate_strings
+    @decks_global_strings = PopulateStrings.new(deck_strings).call
   end
 end
