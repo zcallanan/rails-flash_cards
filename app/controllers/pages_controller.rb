f7d102d837c5ba8aaf02d40203c1c17cea49837a
Zach Callanan <caveofmonsters@gmail.com>
2020-07-02 15:27:19 +0200

    added nested multi-select for categories, selection of a language, and updated populate_strings to handle no user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
+  include Languages
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @languages = Languages.list
     @category = Category.new
     @categories = Category.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_082128) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "theme"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
