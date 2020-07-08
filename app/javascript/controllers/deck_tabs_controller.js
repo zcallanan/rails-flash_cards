8396974c80783960c587ba268e9c8d2aec103b67
Zach Callanan <caveofmonsters@gmail.com>
2020-07-08 02:06:54 +0200

    deck tabs now functional

--- app/javascript/controllers/deck_tabs_controller.js
+++ app/javascript/controllers/deck_tabs_controller.js
@@ -2,44 +2,45 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [
-    'alltab',
-    'mytab',
-    'sharedtab',
     'global',
     'mydecks',
     'shared',
     'listall',
     'listmydecks',
-    'listshared'
+    'listshared',
+    'linkall',
+    'linkmydecks',
+    'linkshared'
   ]
 
   tabclick(event) {
-    const alltab = this.alltabTarget;
-    const mytab = this.mytabTarget;
-    const sharedtab = this.sharedtabTarget;
+    event.preventDefault();
+
     const global_div = this.globalTarget;
     const mydecks_div = this.mydecksTarget;
     const shared_div = this.sharedTarget;
     const list_all = this.listallTarget;
     const list_my_decks = this.listmydecksTarget;
     const list_shared = this.listsharedTarget;
-    console.log(list_all.classList)
+    const linkall = this.linkallTarget;
+    const linkmydecks = this.linkmydecksTarget;
+    const linkshared = this.linksharedTarget;
 
-    if (event.target == list_all && !list_all.classList.contains('active')) {
+    if ((event.target == list_all || event.target == linkall) && !list_all.classList.contains('active')) {
       global_div.style.display = 'block';
       mydecks_div.style.display = 'none';
       shared_div.style.display = 'none';
       if (list_my_decks.classList.contains('active')) list_my_decks.classList.remove('active')
       if (list_shared.classList.contains('active')) list_shared.classList.remove('active')
       list_all.classList.add('active');
-    } else if (event.target == list_my_decks && !list_my_decks.classList.contains('active')) {
+    } else if ((event.target == list_my_decks || event.target == linkmydecks) && !list_my_decks.classList.contains('active')) {
       global_div.style.display = 'none';
       mydecks_div.style.display = 'block';
       shared_div.style.display = 'none';
       if (list_all.classList.contains('active')) list_all.classList.remove('active')
       if (list_shared.classList.contains('active')) list_shared.classList.remove('active')
       list_my_decks.classList.add('active');
-    } else if (event.target == list_shared && !list_shared.classList.contains('active')) {
+    } else if ((event.target == list_shared || event.target == linkshared) && !list_shared.classList.contains('active')) {
       global_div.style.display = 'none';
       mydecks_div.style.display = 'none';
       shared_div.style.display = 'block';
