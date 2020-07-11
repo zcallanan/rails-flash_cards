c0b12ee8944fa1b78bf3af8873cdcc0ef9ed4208
Zach Callanan <caveofmonsters@gmail.com>
2020-07-11 23:01:48 +0200

    javascript formatting

--- app/javascript/utils/build_search_url.js
+++ app/javascript/utils/build_search_url.js
@@ -1,4 +1,4 @@
-const buildSearchUrl = (search_url) => {
+const buildSearchUrl = (searchUrl) => {
   let categoryArray = []
   const initCategoryString = '\?category%5Bname%5D%5B%5D=';
   const categoryString = '&category%5Bname%5D%5B%5D=';
@@ -6,11 +6,11 @@ const buildSearchUrl = (search_url) => {
   let categoryValue = '';
   let tagValue = '';
 
-  const languageValue = `&category%5Blanguage%5D=${search_url.language}`;
-  search_url.options.forEach(option => {
+  const languageValue = `&category%5Blanguage%5D=${searchUrl.language}`;
+  searchUrl.options.forEach(option => {
     if (option.selected === true) categoryArray.push(option.value)
   })
-  const tagArray = search_url.tag.split(', ');
+  const tagArray = searchUrl.tag.split(', ');
   categoryArray.forEach(category => {
     if (categoryArray.indexOf(category) === 0) {
       categoryValue = `${categoryValue}${initCategoryString}${category}`
@@ -21,7 +21,7 @@ const buildSearchUrl = (search_url) => {
   tagArray.forEach(tag => {
     tagValue = `${tagValue}${tagString}${tag}`
   })
-  return `${search_url.urlRoute}${search_url.dest}${categoryValue}${languageValue}${tagValue}`
+  return `${searchUrl.urlRoute}${searchUrl.dest}${categoryValue}${languageValue}${tagValue}`
 }
 
 export { buildSearchUrl }
