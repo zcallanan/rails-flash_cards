a49c1d291c0b92401b0da9d8d9be511e1d5c2fb8
Zach Callanan <caveofmonsters@gmail.com>
2020-07-21 22:50:44 +0200

    Moved deck panel search to a sidebar. Fixed sidebar, and updated index-scroll.js to account for it on scroll.

--- app/javascript/controllers/index_scroll_controller.js
+++ app/javascript/controllers/index_scroll_controller.js
@@ -1,16 +1,26 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "nav" ]
+  static targets = [
+    'nav',
+    'panel'
+  ]
 
   fixNav() {
-    const index_nav = this.navTarget
+    const index_nav = this.navTarget;
+    const panel = this.panelTarget;
+    const navHeight = index_nav.getBoundingClientRect().height;
+
     if (index_nav.dataset.topNav !== undefined) {
-      const topOfNav = index_nav.dataset.topNav
+      const topOfNav = index_nav.dataset.topNav;
       if (window.scrollY >= topOfNav) {
         document.body.classList.add('fixed-nav');
+        panel.classList.add('search-side-panel');
+        panel.style.marginTop = -navHeight + 'px';
       } else {
         document.body.classList.remove('fixed-nav');
+        panel.classList.remove('search-side-panel');
+        panel.style.marginTop = 0;
       }
     } else {
       index_nav.dataset.topNav = index_nav.offsetTop;
