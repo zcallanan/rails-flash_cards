63e266e4e62b8a28d9aad067930f7a48dc245fdd
Zach Callanan <caveofmonsters@gmail.com>
2020-07-24 00:18:48 +0200

    adjusted layout to eliminate need for index_scroll.js

--- app/javascript/controllers/index_scroll_controller.js
+++ app/javascript/controllers/index_scroll_controller.js
@@ -10,26 +10,28 @@ export default class extends Controller {
 
   fixNav() {
     const index_nav = this.navTarget;
+    console.log(index_nav.offsetTop)
     const searchPanel = this.searchPanelTarget;
     const deckList = this.deckListTarget;
     const deckCreate = this.deckCreateTarget;
     const navHeight = index_nav.getBoundingClientRect().height;
+     console.log(navHeight)
 
     if (index_nav.dataset.topNav !== undefined) {
       const topOfNav = index_nav.dataset.topNav;
       if (window.scrollY >= topOfNav) {
-        document.body.classList.add('fixed-nav');
-        searchPanel.style.paddingTop = navHeight + 'px';  // stop side panel from jumping down when the nav is fixed
-        deckList.style.paddingTop = navHeight + 'px'; // stop deck panel/top bar from jumping up when the nav is fixed
-        deckCreate.style.paddingTop = navHeight + 'px';
+        // document.body.classList.add('fixed-nav');
+        // searchPanel.style.paddingTop = navHeight + 'px';  // stop side panel from jumping down when the nav is fixed
+        // deckList.style.paddingTop = navHeight + 'px'; // stop deck panel/top bar from jumping up when the nav is fixed
+        // deckCreate.style.paddingTop = navHeight + 'px';
       } else {
-        document.body.classList.remove('fixed-nav');
-        searchPanel.style.paddingTop = 0;
-        deckCreate.style.paddingTop = 0;
-        deckList.style.paddingTop = 0;
+        // document.body.classList.remove('fixed-nav');
+        // searchPanel.style.paddingTop = 0;
+        // deckCreate.style.paddingTop = 0;
+        // deckList.style.paddingTop = 0;
       }
     } else {
-      index_nav.dataset.topNav = index_nav.offsetTop;
+      index_nav.dataset.topNav = index_nav.offsetTop + navHeight;
     }
   }
 }
